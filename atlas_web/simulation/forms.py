67ca1143eab823bb5578a7d2ac7262d2af1f6ff1
Migelo <miha@filetki.si>
2020-11-17 01:59:47 +0100

    early implementation of simulation/models.py

--- atlas_web/simulation/forms.py
+++ atlas_web/simulation/forms.py
@@ -8,10 +8,58 @@ class SimulationForm(forms.Form):
         ("asplund", "Asplund"),
         ("grevess", "Grevess"),
     ]
+    TEMPERATURE_CHOICES = [("standard", "Standard"), ("non-standard", "Non-standard")]
+    PRESSURE_CHOICES = [("standard", "Standard"), ("non-standard", "Non-standard")]
 
     name = forms.CharField(required=True)
     abundances = forms.ChoiceField(
         label=_("Abundances"), required=True, choices=ABUNDANCE_CHOICES
+    )
+
+    metallicity = forms.FloatField(
+        required=True,
+        min_value=-4,
+        max_value=2,
+        widget=forms.NumberInput(attrs={"id": "form_metallicity", "step": "0.1"}),
+        label=_("Metallicity in units of solar metallicity"),
+    )
+    T_eff = forms.FloatField(
+        required=True,
+        min_value=1500,
+        max_value=12000,
+        widget=forms.NumberInput(attrs={"id": "form_T_eff", "step": "100"}),
+        label=_("Effective temperature of the model"),
+    )
+    log_G = forms.FloatField(
+        required=True,
+        min_value=0,
+        max_value=5,
+        widget=forms.NumberInput(attrs={"id": "form_log_G", "step": ".1"}),
+        label=_("log G of the model"),
+    )
+    vturb = forms.FloatField(
+        required=True,
+        min_value=0,
+        max_value=5,
+        widget=forms.NumberInput(attrs={"id": "form_vturb", "step": ".1"}),
+        label=_("Turbulent Velocity in km/s"),
+    )
+
+    convection = forms.BooleanField(label=_("Enable Convection?"), required=True)
+    mixing_length = forms.FloatField(
+        required=False,
+        min_value=0,
+        max_value=5,
+        widget=forms.NumberInput(attrs={"id": "form_mixing_length", "step": "1"}),
+        label=_("Mixing length in km, only used when convection is enabled."),
+    )
+    overshoot = forms.BooleanField(label=_("Include overshoot?"))
+    wavelength_grid = forms.ChoiceField(label=_("Wavelength grid"), required=True)
+    T_grid = forms.ChoiceField(
+        label=_("Temperature grid"), required=True, choices=TEMPERATURE_CHOICES
+    )
+    p_grid = forms.ChoiceField(
+        label=_("Pressure grid"), required=True, choices=PRESSURE_CHOICES
     )
 
     # TODO: clean method can check the form against validators, and e.g.
@@ -19,6 +67,23 @@ class SimulationForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         name = cleaned_data.get("name")
+        convection = cleaned_data.get("convection")
+        mixing_length = cleaned_data.get("mixing_length")
+        overshoot = cleaned_data.get("overshoot")
 
         if name != "Miha":
             raise forms.ValidationError(_("Your name has to be Miha"))
+        if not convection:
+            if mixing_length is not None:
+                raise forms.ValidationError(
+                    _("Mixing length is only used when convection is enabled.")
+                )
+            if overshoot:
+                raise forms.ValidationError(
+                    _("Overshoot is only used when convection is enabled.")
+                )
+        if convection:
+            if mixing_length is None:
+                raise forms.ValidationError(
+                    _("Missing mixing length when convection is enabled.")
+                )
--- atlas_web/simulation/utils.py
+++ atlas_web/simulation/utils.py
@@ -1,4 +1,5 @@
 from atlas_web.simulation.abundances import ANDERS, ASPLUND, GREVESS
+from atlas_web.simulation.grids import P_GRID, T_GRID, WAVELENGTH_GRID
 
 
 def create_atlas_input_file_from_valid_form(cleaned_data):
@@ -16,11 +17,43 @@ def create_atlas_input_file_from_valid_form(cleaned_data):
         print("We will use grevess abunances")
         abundances_to_use = GREVESS
     else:
-        pass
-        # raise SomethingError
+        raise
+
+    if cleaned_data["wavelength_grid"] == "standard":
+        print("We will use standard wavelength grid")
+        wavelength_grid_to_use = WAVELENGTH_GRID
+    elif cleaned_data["wavelength_grid"] == "non-standard":
+        print("We will use non-standard wavelength grid")
+        raise NotImplementedError
+    else:
+        raise
+
+    if cleaned_data["T_grid"] == "standard":
+        print("We will use standard temperature grid")
+        T_grid_to_use = T_GRID
+    elif cleaned_data["T_grid"] == "non-standard":
+        print("We will use non-standard temperature grid")
+        raise NotImplementedError
+    else:
+        raise
+
+    if cleaned_data["p_grid"] == "standard":
+        print("We will use standard pressure grid")
+        P_grid_to_use = P_GRID
+    elif cleaned_data["p_grid"] == "non-standard":
+        print("We will use non-standard pressure grid")
+        raise NotImplementedError
+    else:
+        raise
 
     fname = "tmp.txt"
     with open(fname, "w") as f:
         f.write("We use the following abunances\n")
         f.write("\n".join(str(value) for value in abundances_to_use))
+        f.write("We use the following wavelength grid\n")
+        f.write("\n".join(str(value) for value in wavelength_grid_to_use))
+        f.write("We use the following T grid\n")
+        f.write("\n".join(str(value) for value in T_grid_to_use))
+        f.write("We use the following p grid\n")
+        f.write("\n".join(str(value) for value in P_grid_to_use))
     return fname
